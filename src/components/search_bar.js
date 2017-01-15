f384fa15e0db5fe425a8f919b6787281f8f9d716
vedant koditkar <vedant.koditkar.vk@gmail.com>
2017-01-15 17:14:32 +0530

    updated component from function based to class based

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />;   // React.createElement
-};
+// declare component using function
+//const SearchBar = () => {
+//  return <input />;   // React.createElement
+//};
+
+// declare component using class based component
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
 
 export default SearchBar;
