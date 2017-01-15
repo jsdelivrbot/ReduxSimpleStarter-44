7c31a0f26b131de990ef70f3011ed03046698e50
vedant koditkar <vedant.koditkar.vk@gmail.com>
2017-01-15 19:27:24 +0530

    Controlled Input component Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // declare component using function
 //const SearchBar = () => {
@@ -6,9 +6,28 @@ import React from 'react';
 //};
 
 // declare component using class based component
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+      <input
+        id="demo"
+        value={this.state.term}
+        onChange = { this.onInputChange }
+        />
+      </div>
+    );
+  }
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
   }
 }
 
