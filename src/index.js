939004daec7c769cec3460f6e670b4f1fad2983d
vedant koditkar <vedant.koditkar.vk@gmail.com>
2017-01-15 19:48:08 +0530

    Changed the parent component into class based component and added the initial call to api in constructor and assigned it to the state.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyBWkyIAQOSeJidl-bwP3KL-isyDsngnkDc";
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+      // it will only work if key and value is same
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
